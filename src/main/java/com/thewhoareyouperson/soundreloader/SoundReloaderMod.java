b275546cc9240041ad16b18f334e7e65341157c6
TheWhoAreYouPerson <thewhoareyouperson@gmail.com>
2016-11-25 23:56:30 -0600

    Added version checking for less future version modifications, and 1.7.10 changes

--- src/main/java/com/thewhoareyouperson/soundreloader/SoundReloaderMod.java
+++ src/main/java/com/thewhoareyouperson/soundreloader/SoundReloaderMod.java
@@ -14,19 +14,47 @@ import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.init.Blocks;
 import net.minecraft.launchwrapper.Launch;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.client.registry.ClientRegistry;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.InputEvent;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.client.FMLClientHandler;
+import cpw.mods.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.InputEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 @Mod(modid = SoundReloaderMod.MODID, version = SoundReloaderMod.VERSION)
 public class SoundReloaderMod {
+	static {
+		String tmpMcVersion;
+		{ //Get the MC version in a way that doesn't make it a compile time constant
+			Field fldMcVersion;
+			try {
+				fldMcVersion = MinecraftForge.class.getField("MC_VERSION");
+			} catch (Exception e) {
+				fldMcVersion = null;
+			}
+			
+			try {
+				tmpMcVersion = (String) fldMcVersion.get(null);
+			} catch (IllegalArgumentException e) {
+				tmpMcVersion = null;
+			} catch (IllegalAccessException e) {
+				tmpMcVersion = null;
+			}
+		}
+		mcVersion = tmpMcVersion;
+	}
+	
 	public static final String MODID = "soundreloader";
 	public static final String VERSION = "1.0";
 	public static final Logger Logger = LogManager.getLogger(SoundReloaderMod.MODID);
+	public static final String mcVersion;
 	
 	protected static final boolean devEnv = (Boolean) Launch.blackboard.get("fml.deobfuscatedEnvironment");
 	
@@ -34,9 +62,20 @@ public class SoundReloaderMod {
 	
 	@EventHandler
 	public void preinit(FMLPreInitializationEvent event) {
+		Logger.info("Registering Keybindings");
+		Logger.info("Minecraft version: '" + mcVersion + "'");
+		if(devEnv)
+			Logger.info("Running in Development (Deobfuscated) Environment");
+		
 		soundBinding = new KeyBinding("Reload Default Sound", Keyboard.KEY_P, "key.categories.misc");
 		ClientRegistry.registerKeyBinding(soundBinding);
-		MinecraftForge.EVENT_BUS.register(new KeyInputHandler());
+		if("1.7.10".equals(mcVersion)){
+			FMLCommonHandler.instance().bus().register(new KeyInputHandler());
+		} else if("1.8.9".equals(mcVersion) || "1.10.2".equals(mcVersion)) { //Others. Namely, 1.8.9 and 1.10.2
+			MinecraftForge.EVENT_BUS.register(new KeyInputHandler());
+		} else {
+			Logger.warn("Unknown MC Version " + mcVersion + ", not initializing keybind handler.");
+		}
 	}
 	
 	public static class KeyInputHandler {
@@ -51,9 +90,14 @@ public class SoundReloaderMod {
 		        	SoundManager mngr = null;
 		        	Exception ex = null;
 		        	try {
-						for(Field fd : SoundHandler.class.getDeclaredFields())
-							Logger.info("SoundHandler Field: '" + fd.getName() + "' of type '" + fd.getType() + "'");
-						Field fi = SoundHandler.class.getDeclaredField(devEnv ? "sndManager" : "field_147694_f");// mcpbot says field_148622_c but MC (code above) says field_147694_f
+		        		Field fi;
+		        		if(devEnv){
+							for(Field fd : SoundHandler.class.getDeclaredFields())
+								Logger.info("SoundHandler Field: '" + fd.getName() + "' of type '" + fd.getType() + "'");
+							fi = SoundHandler.class.getDeclaredField("sndManager");// mcpbot says field_148622_c but MC (code above) says field_147694_f
+		        		} else {
+		        			fi = SoundHandler.class.getDeclaredField("field_147694_f");// mcpbot says field_148622_c but MC (code above) says field_147694_f
+		        		}
 						fi.setAccessible(true);
 						mngr = (SoundManager) fi.get(handler);
 					} catch (NoSuchFieldException e) {
